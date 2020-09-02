df2e24fd623ae9db7baa803fbafc627c340ecf7e
lynyrd-designer <larki74@yandex.ru>
2020-09-02 12:13:30 +0500

    'start'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+import Layout from "./hoc/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <div>
+            <h1>Layout components</h1>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
